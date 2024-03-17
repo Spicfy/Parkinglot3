373db95a85fd8003bafaff0dc64d09a6cbaab3c0
Spicfy <68024083+Spicfy@users.noreply.github.com>
2024-03-17 00:39:09 -0400

    added time and print message after every simulation run

--- CapacityOptimizer.java
+++ CapacityOptimizer.java
@@ -6,14 +6,20 @@ public class CapacityOptimizer {
 	public static int getOptimalNumberOfSpots(int hourlyRate) {
 		int n = 1;
 		int sum = 0;
+		System.out.println("==== Setting lot capacity to: " + n + "====");
 		for(int i = 1; i <= NUM_RUNS; i++){
+			long start = System.currentTimeMillis();
 			ParkingLot lot = new ParkingLot(n);
 			Simulator sim = new Simulator(lot, hourlyRate,86400);
 			sim.simulate();
 			int queueSize = sim.getIncomingQueueSize();
+			long end = System.currentTimeMillis();
+
+			System.out.println("Simulation run "+ n + "(" + (end - start)+"); Queue length at the end of simulation run: "+ queueSize);
+
 			sum += queueSize;
 		}
-		double avg = sum/10;
+		double avg = sum/NUM_RUNS;
 		if (avg <= THRESHOLD){
 			return n;
 		}
